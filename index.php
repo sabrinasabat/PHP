69d2c259772a19fd4db8ee70a2f67003acf5230b
sabrinasabat <sabrinasabats@gmail.com>
2024-10-23 16:54:58 +0200

    23.10.24

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+    require_once 'funcions.php';
+
+    // Dades inicials (exemple)
+    //Creando productos:
+    $producto1 = crearProducte('Samarreta', 'Samarreta de cotó', 19.99);
+    $producto2 = crearProducte('Pantalons', 'Pantalons vaquer', 39.99);
+    $producto3 = crearProducte('Jaqueta', 'Corta vento', 35.00);
+    $producto4 = crearProducte('Vaquero', 'Piernas cortas', 49.50);
+
+    //Creando categorías:
+    $categoria1 = crearCategoria('Roba', 'Secció de roba');
+    $categoria2 = crearCategoria('Home', 'Productes per a home');
+    $categoria3 = crearCategoria('Jaquetas', 'Corta vento');
+
+    //Agregando categoría al producto
+    agregarCategoriaAProducte($producto1, $categoria1);
+    agregarCategoriaAProducte($producto1, $categoria2);
+    agregarCategoriaAProducte($producto2, $categoria1);
+    agregarCategoriaAProducte($producto3, $categoria3);
+    agregarCategoriaAProducte($producto3, $categoria1);
+    agregarCategoriaAProducte($producto4, $categoria1);
+
+    //Validación de errores:
+    //$producto5 = crearProducte('', 'amem', 74.80);
+
+
+    // Mostrar productes
+    $productos = [$producto1, $producto2, $producto3, $producto4];
+    //mostrarProductes($productos);
+
+    //$productesRoba = obtenirProductsPorCategoria($categoria1, $productos);
+    //mostrarProductes($productesRoba);
+
+    //$productesJaquetas = obtenirProductsPorCategoria($categoria3, $productos);
+    //mostrarProductes($productesJaquetas);
+
+    include "template.php";
+
+?>
